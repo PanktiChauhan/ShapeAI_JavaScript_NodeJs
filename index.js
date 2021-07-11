01800d4cf0c35fe8e06d30de9426082613eee1c5
Pankti Chauhan <84937263+PanktiChauhan@users.noreply.github.com>
2021-07-11 11:27:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8080;
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+var age = (Number(req.body.n1 )/15 );
+ res.send("Your age in Dog years is : " + age +" Dog years." )
+});
+
+
+app.listen(port);
